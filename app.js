93917d04a64597c2eb6220db4372daa795708ce0
Vajid <alvajindia@gmail.com>
2017-02-24 11:00:29 +0530

    commit for first push - structuring WIP

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var passport = require('passport-local');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +15,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
