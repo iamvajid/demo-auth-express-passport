3a8dd6e83bfdab8438da386640459fa26318a5c9
Vajid <alvajindia@gmail.com>
2017-02-24 18:14:47 +0530

    adding configs

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 
 var express = require('express');
+var app = express();
+
 var path = require('path');
+var flash  = require('connect-flash');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
-var passport = require('passport-local');
+var mongoose = require('mongoose');
+var passport = require('passport');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var env = require('./config/environments.js')
+var config_db = require('./config/database.js');
+mongoose.connect(configDB.url); // connect to our database
 
-var app = express();
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 
+app.use(express.static(path.join(__dirname, 'public')));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,10 +31,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(session({ secret: 'anysecretissecrettillexposed' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,10 +52,13 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = app;
+
+//Launch the server
+app.listen('80');
+console.log("Server is starting !");
+
